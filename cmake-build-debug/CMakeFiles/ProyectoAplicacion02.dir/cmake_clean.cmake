7fe220e91c578a1ef12f42cb44168e99cc826875
Smorareq1 <smorareq1@gmail.com>
2023-10-30 21:42:44 -0600

    Prueba No.03

---
+++ cmake-build-debug/CMakeFiles/ProyectoAplicacion02.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoAplicacion02.dir/main.cpp.obj"
+  "CMakeFiles/ProyectoAplicacion02.dir/main.cpp.obj.d"
+  "ProyectoAplicacion02.exe"
+  "ProyectoAplicacion02.exe.manifest"
+  "ProyectoAplicacion02.pdb"
+  "libProyectoAplicacion02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoAplicacion02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
